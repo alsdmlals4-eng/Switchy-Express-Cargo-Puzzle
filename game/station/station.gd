082efb76824b0abe2286a11a596dd99126f6cbd5
alsdmlals4-eng <alsdmlals4@gmail.com>
2026-08-01 10:24:25 +0900

    feat: return LIFO unload result ViewModels

--- game/station/station.gd
+++ game/station/station.gd
@@ -11,10 +11,17 @@ func _init(station_cell: Vector2i = Vector2i.ZERO, accepted_type: StringName = &
 
 
 func try_unload(cargo_stack: Variant) -> Dictionary:
+	var unload_order_before: Array = cargo_stack.unload_order()
+	var top_before: StringName = cargo_stack.peek()
 	var unloaded: Array = cargo_stack.pop_matching_group(cargo_type)
+	var unload_order_after: Array = cargo_stack.unload_order()
 	return {
 		"station_cell": cell,
 		"cargo_type": cargo_type,
+		"top_before": top_before,
+		"matched": not unloaded.is_empty(),
 		"items": unloaded,
 		"count": unloaded.size(),
+		"unload_order_before": unload_order_before,
+		"unload_order_after": unload_order_after,
 	}
