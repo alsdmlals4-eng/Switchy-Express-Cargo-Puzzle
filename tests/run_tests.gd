1d99efa107cf0bf6023c5ccefd61ce0a0783896c
alsdmlals4-eng <alsdmlals4@gmail.com>
2026-08-03 23:18:33 +0900

    test: register R1 pressure snapshot RED suite

--- tests/run_tests.gd
+++ tests/run_tests.gd
@@ -27,6 +27,7 @@ const TEST_SCRIPTS: Array[Script] = [
 	preload("res://tests/run/test_run_balance.gd"),
 	preload("res://tests/run/test_run_state.gd"),
 	preload("res://tests/difficulty/test_difficulty_director.gd"),
+	preload("res://tests/difficulty/test_difficulty_pressure_schedule.gd"),
 	preload("res://tests/run/test_run_controller.gd"),
 	preload("res://tests/run/test_run_controller_difficulty_events.gd"),
 	preload("res://tests/run/test_run_controller_guards.gd"),
