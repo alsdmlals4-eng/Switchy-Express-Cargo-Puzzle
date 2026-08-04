1c0eaac9368e0d56f2e32e3ab8e9a3b6a19bc1e0
alsdmlals4-eng <alsdmlals4@gmail.com>
2026-08-05 01:43:16 +0900

    test: register failing finite proof build session test

--- tests/run_tests.gd
+++ tests/run_tests.gd
@@ -18,6 +18,7 @@ const TEST_SCRIPTS: Array[Script] = [
 	preload("res://tests/finite/build/test_track_layout_editor.gd"),
 	preload("res://tests/finite/rail/test_finite_track_graph.gd"),
 	preload("res://tests/finite/build/test_preflight_validator.gd"),
+	preload("res://tests/finite/integration/test_finite_build_session.gd"),
 	preload("res://tests/train/test_train_movement.gd"),
 	preload("res://tests/train/test_compact_wagon_tokens.gd"),
 	preload("res://tests/train/test_train_footprint.gd"),
