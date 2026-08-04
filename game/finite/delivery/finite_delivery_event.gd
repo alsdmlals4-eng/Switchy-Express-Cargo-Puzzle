bfe13af78191bdc2de1a02ced55782462d4f8239
alsdmlals4-eng <alsdmlals4@gmail.com>
2026-08-05 07:58:52 +0900

    fix: make finite delivery events immutable

--- game/finite/delivery/finite_delivery_event.gd
+++ game/finite/delivery/finite_delivery_event.gd
@@ -1,15 +1,69 @@
 class_name FiniteDeliveryEvent
 extends RefCounted
 
-var cell: Vector2i = Vector2i.ZERO
-var event_time: float = 0.0
-var picked_up: bool = false
-var pickup_type: StringName = &""
-var unload_count: int = 0
-var unloaded_items: Array[StringName] = []
-var stop_requested: bool = false
-var remaining_map_cargo: int = 0
-var stack_size: int = 0
+var _cell: Vector2i = Vector2i.ZERO
+var _event_time: float = 0.0
+var _picked_up: bool = false
+var _pickup_type: StringName = &""
+var _unload_count: int = 0
+var _unloaded_items: Array[StringName] = []
+var _stop_requested: bool = false
+var _remaining_map_cargo: int = 0
+var _stack_size: int = 0
+
+var cell: Vector2i:
+	get:
+		return _cell
+	set(_value):
+		pass
+
+var event_time: float:
+	get:
+		return _event_time
+	set(_value):
+		pass
+
+var picked_up: bool:
+	get:
+		return _picked_up
+	set(_value):
+		pass
+
+var pickup_type: StringName:
+	get:
+		return _pickup_type
+	set(_value):
+		pass
+
+var unload_count: int:
+	get:
+		return _unload_count
+	set(_value):
+		pass
+
+var unloaded_items: Array[StringName]:
+	get:
+		return _unloaded_items.duplicate()
+	set(_value):
+		pass
+
+var stop_requested: bool:
+	get:
+		return _stop_requested
+	set(_value):
+		pass
+
+var remaining_map_cargo: int:
+	get:
+		return _remaining_map_cargo
+	set(_value):
+		pass
+
+var stack_size: int:
+	get:
+		return _stack_size
+	set(_value):
+		pass
 
 
 func _init(
@@ -22,12 +76,12 @@ func _init(
 	map_cargo_remaining: int = 0,
 	current_stack_size: int = 0
 ) -> void:
-	cell = event_cell
-	event_time = timestamp
-	picked_up = did_pick_up
-	pickup_type = picked_type
-	unloaded_items = items_unloaded.duplicate()
-	unload_count = unloaded_items.size()
-	stop_requested = should_stop
-	remaining_map_cargo = map_cargo_remaining
-	stack_size = current_stack_size
+	_cell = event_cell
+	_event_time = timestamp
+	_picked_up = did_pick_up
+	_pickup_type = picked_type
+	_unloaded_items = items_unloaded.duplicate()
+	_unload_count = _unloaded_items.size()
+	_stop_requested = should_stop
+	_remaining_map_cargo = map_cargo_remaining
+	_stack_size = current_stack_size
